cd7c2849b4db71c69c16a7abe8b8a01c7e250230
Aleksandr Berkhmiller <al.berhmiller@gmail.com>
2025-12-19 15:30:48 +0700

    Implement append stdout

--- src/output.rs
+++ src/output.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Write, path::PathBuf};
+use std::{fs::OpenOptions, io::Write, path::PathBuf};
 
 #[derive(Debug)]
 pub enum RedirectType<'a> {
@@ -6,6 +6,9 @@ pub enum RedirectType<'a> {
     Stdout(&'a [&'a str]),
     StdoutOne(&'a [&'a str]),
     StdErr(&'a [&'a str]),
+    Append(&'a [&'a str]),
+    AppendStdout(&'a [&'a str]),
+    AppendStderr(&'a [&'a str]),
 }
 
 pub fn handle_output(output: String, output_err: String, redirect: RedirectType) {
@@ -18,22 +21,43 @@ pub fn handle_output(output: String, output_err: String, redirect: RedirectType)
             let path_arg = String::from(args[0]);
             let path = PathBuf::from(path_arg);
             if output_err.is_empty() {
-                file_output(output, path)
+                file_output(output, path, false)
             } else {
-                file_output(output_err, path)
+                file_output(output_err, path, false)
             }
         }
         RedirectType::StdoutOne(args) => {
             let path_arg = String::from(args[0]);
             let path = PathBuf::from(path_arg);
-            file_output(output, path);
+            file_output(output, path, false);
             console_output(output_err);
         }
         RedirectType::StdErr(args) => {
             let path_arg = String::from(args[0]);
             let path = PathBuf::from(path_arg);
-            file_output(output_err, path);
+            file_output(output_err, path, false);
             console_output(output);
+        }
+        RedirectType::Append(args) => {
+            let path_arg = String::from(args[0]);
+            let path = PathBuf::from(path_arg);
+            if output_err.is_empty() {
+                file_output(output, path, true)
+            } else {
+                file_output(output_err, path, true)
+            }
+        }
+        RedirectType::AppendStderr(args) => {
+            let path_arg = String::from(args[0]);
+            let path = PathBuf::from(path_arg);
+            file_output(output_err, path, true);
+            console_output(output);
+        }
+        RedirectType::AppendStdout(args) => {
+            let path_arg = String::from(args[0]);
+            let path = PathBuf::from(path_arg);
+            file_output(output, path, true);
+            console_output(output_err);
         }
     }
 }
@@ -51,17 +75,24 @@ pub fn separare_redirect_and_args<'a>(args: &'a [&'a str]) -> (&'a [&'a str], Re
 
             let chars_count = args[idx].len();
 
-            if chars_count == 1 {
-                return (real_args, RedirectType::Stdout(rest));
-            } else if chars_count == 2 {
-                let redirect_str = args[idx].to_owned();
-                match redirect_str.chars().next().unwrap() {
-                    '1' => return (real_args, RedirectType::StdoutOne(rest)),
-                    '2' => return (real_args, RedirectType::StdErr(rest)),
-                    _ => return (real_args, RedirectType::Stdout(rest)),
+            match chars_count {
+                1 => return (real_args, RedirectType::Stdout(rest)),
+                2 => {
+                    let redirect_str = args[idx].to_owned();
+                    match redirect_str.chars().next().unwrap() {
+                        '1' => return (real_args, RedirectType::StdoutOne(rest)),
+                        '2' => return (real_args, RedirectType::StdErr(rest)),
+                        '>' => return (real_args, RedirectType::Append(rest)),
+                        _ => {}
+                    }
                 }
+                3 => match args[idx] {
+                    "1>>" => return (real_args, RedirectType::AppendStdout(rest)),
+                    "2>>" => return (real_args, RedirectType::AppendStderr(rest)),
+                    _ => {}
+                },
+                _ => {}
             }
-
             (real_args, RedirectType::Stdout(rest))
         }
     }
@@ -73,17 +104,25 @@ fn console_output(mut output: String) {
     }
 
     // FIXME: Need to real handle quotes
-    output = output.replace("'", "");
+    output = output.replace("'", "").replace('"', "");
     print!("{}", output);
     if !output.ends_with("\n") {
         println!();
     }
 }
 
-fn file_output(output: String, path: PathBuf) {
+fn file_output(mut output: String, path: PathBuf, append: bool) {
+    output = output.replace("'", "").replace('"', "");
+
+    if !output.ends_with("\n") && !output.is_empty() {
+        output.push('\n');
+    }
+    let mut options = OpenOptions::new();
+    options.write(true).append(append);
+
     let mut file = match path.exists() {
-        true => File::open(path).unwrap(),
-        false => File::create_new(path).unwrap(),
+        true => options.open(path).unwrap(),
+        false => options.create_new(true).open(path).unwrap(),
     };
 
     let _ = file.write_all(&output.into_bytes());
